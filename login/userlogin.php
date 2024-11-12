494c2541e847e29ee0e2430cc4a732a6dde1be80
archanatimilsina <archanatimilsina88@gmail.com>
2024-11-12 09:02:55 +0545

    11-12

--- login/userlogin.php
+++ login/userlogin.php
@@ -13,6 +13,9 @@ session_start();
     <?php 
     if(isset($_POST['submit']))
     {
+        //check->
+        echo '<script>alert("form submitted")</script>';
+        //<-check
         $email_username=$_POST['email_username'];
         $password=$_POST['password'];
       $select_query="SELECT * from users where email='$email_username' OR username='$email_username'";
@@ -20,19 +23,29 @@ session_start();
       $count=mysqli_num_rows($select_result);
       if($count!=0)
       {
+         //check->
+         echo '<script>alert("Data is in Database")</script>';
+         //<-check
 $data=mysqli_fetch_array($select_result);
 if(password_verify($password,$data['password']))
 {
+     //check->
+     echo '<script>alert("password verified")</script>';
+     //<-check
     $_SESSION['userpanel']['logged_in']=true;
     $_SESSION['userpanel']['username']=$data['username'];
     $_SESSION['userpanel']['profile']=$data['profile'];
     $_SESSION['userpanel']['fullname']=$data['fullname'];
     $_SESSION['userpanel']['account_type']='user';
     $_SESSION['userpanel']['email']=$data['email'];
- 
+   //check->
+   echo '<script>alert("You are logged in congratulation happy birthday");
+   window.location.href="../index.php"; 
+   </script>';
+   //<-check
     ?>
     <script>
-window.location.href='../index.php'; 
+
 </script>
 <?php
 }
